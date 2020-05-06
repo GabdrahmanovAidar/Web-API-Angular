4dd75bd44f92169007bb1d4547556c680dfe77df
Aidar <aidar05ss@mail.ru>
2020-05-06 14:26:56 +0300

    UploadPhoto

--- api/Web.API.Models/UploadModel.cs
+++ api/Web.API.Models/UploadModel.cs
@@ -4,7 +4,7 @@
     {
         public string Id { get; set; }
         public string Extension { get; set; }
-        public string Source => $"https://327517.selcdn.ru/darkxx/uploads/%7BId%7D%7BExtension%7D";
+        public string Source => $"https://415705.selcdn.ru/RANStudy/upload/{Id}{Extension}";
         public int? SizeInBytes { get; set; }
     }
 }
